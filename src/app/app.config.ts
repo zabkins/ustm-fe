ad95e435ba62f70d75316544f1db0389c0a71c7e
mzarczynski <mzarczynski29@gmail.com>
2024-12-30 16:24:46 +0100

    Adds rolling-out animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,12 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from "@angular/common/http";
 import {authInterceptorFn} from "./auth/auth.interceptor.fn";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptorFn]))]
+    provideHttpClient(withInterceptors([authInterceptorFn])),
+    provideAnimations()]
 };
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,11 +1,60 @@
 import { Component } from '@angular/core';
+import {NgClass} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    FormsModule
+  ],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
+  animations: [
+    trigger('rollout', [
+      state(
+        'hidden',
+        style({
+          height: '0',
+          opacity: '0',
+          overflow: 'hidden',
+        })
+      ),
+      state(
+        'visible',
+        style({
+          height: '*',
+          opacity: '1',
+          overflow: 'hidden',
+        })
+      ),
+      transition('hidden <=> visible', [
+        animate('0.4s ease-in-out'),
+      ]),
+    ]),
+    trigger('round-button', [
+      state(
+        'unrounded',
+        style({
+          'border-bottom-right-radius': '0',
+        })
+      ),
+      state(
+        'rounded',
+        style({
+          'border-bottom-right-radius': '0.6rem',
+        })
+      ),
+      transition('unrounded => rounded', [
+        animate('0.5s ease-in'),
+      ]),
+      transition('rounded => unrounded', [
+        animate('0.2s ease-out'),
+      ])
+    ])
+  ]
 })
 export class TaskComponent {
   subtasksVisible = false;
